ba29f1bd75894ec154d19450d833af7c5728f34e
eugeniahwd <eugeniahwda@gmail.com>
2025-04-05 12:29:29 +0700

    edit text

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ st.title("Face and Eye Detection with YOLO")
 uploaded_file = st.file_uploader("Upload a photo", type=["jpg", "png", "jpeg"])
 
 if uploaded_file:
+    st.subheader("Original Image", divider="violet")
     img = Image.open(uploaded_file).convert("RGB")
-    st.image(img, caption="Original Image", use_container_width=True)
+    st.image(img,  use_container_width=True)
 
+    st.subheader("Original Image", divider="blue")
     result_img = detect_faces_and_eyes(np.array(img))
-    st.image(result_img, caption="Detected Image", use_container_width=True)
+    st.image(result_img,  use_container_width=True)
